270194a759489751c3a6ab4beea8b48318f74fcb
Brian Jagodnik <brianjag@mail.mta.ac.il>
2021-04-08 18:48:11 -0300

    Handle an Action in the Store

---
+++ front-end-libraries/redux/handle-an-action-in-the-store.js
@@ -0,0 +1,15 @@
+const defaultState = {
+  login: false
+};
+
+const reducer = (state = defaultState, action) => {
+  return action.type == 'LOGIN' ? { login: true } : state;
+};
+
+const store = Redux.createStore(reducer);
+
+const loginAction = () => {
+  return {
+    type: 'LOGIN'
+  };
+};
